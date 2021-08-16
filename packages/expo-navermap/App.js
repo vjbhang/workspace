3a665e0ca8038f39c06f155f050daed4b0b8295f
unknown <bhangjiwon@gmail.com>
2021-08-16 22:01:11 +0900

    added webview for naver map
    - npm i react-native-webview
    - const map with html string value

--- packages/expo-navermap/App.js
+++ packages/expo-navermap/App.js
@@ -2,10 +2,35 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import styled from "styled-components";
 import { StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
 
 // component
 import Header from "./component/header";
 import MapContainer from "./component/map-container";
+
+const map = `
+  <html>
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+      <title>간단한 지도 표시하기</title>
+      <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=1zi9sb2qmn"></script>
+  </head>
+  <body>
+  <div id="map" style="width:100%;height:100%;"></div>
+
+  <script>
+  var mapOptions = {
+      center: new naver.maps.LatLng(37.3595704, 127.105399),
+      zoom: 10
+  };
+
+  var map = new naver.maps.Map('map', mapOptions);
+  </script>
+  </body>
+  </html>
+`;
 
 export default function App() {
   return (
@@ -13,10 +38,17 @@ export default function App() {
       <StatusBar style="auto" />
       <Header>
         <Text weight="bold" size="18px">
-          naver map in ejected expo
+          naver map in expo using webview
         </Text>
       </Header>
-      <MapContainer></MapContainer>
+      <WebView
+        originWhitelist={["*"]}
+        source={{ html: map }}
+        style={styles.webview}
+      />
+      {/* <MapContainer>
+
+      </MapContainer> */}
     </ScreenContainer>
   );
 }
@@ -24,6 +56,10 @@ export default function App() {
 const styles = StyleSheet.create({
   statusbar: {
     backgroundColor: "#fff",
+  },
+  webview: {
+    height: "100%",
+    width: "100%",
   },
 });
 
