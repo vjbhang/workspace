6ca8aba1e7bfd370777850f1e39c74d47fb115a7
vjbhang <bhangjiwon@pm.me>
2023-08-30 16:25:21 +0900

    text input was not uniform

--- packages/storybook-v1/src/stories/TextInput.tsx
+++ packages/storybook-v1/src/stories/TextInput.tsx
@@ -26,7 +26,7 @@ export const TextInput = ({
           style={inputStyle}
           placeholder={placeholder}
         />
-        <p className="text-xs text-wrap w-full text-red-600">
+        <p className="text-xs text-wrap text-red-600 w-56">
           아이디(로그인 전용 아이디) 또는 비밀번호를 잘못 입력했습니다.입력하신
           내용을 다시 확인해주세요.
         </p>
@@ -34,12 +34,12 @@ export const TextInput = ({
     );
   } else {
     return (
-      <div>
+      <div className="w-56">
         <input
           value={value}
           onChange={(e) => setValue(e.target.value)}
           type="text"
-          className="p-1 rounded-md border-gray-200 border focus:outline-none text-base"
+          className="p-1 w-full rounded-md border-gray-200 border focus:outline-none text-base"
           style={inputStyle}
           placeholder={placeholder}
         />
