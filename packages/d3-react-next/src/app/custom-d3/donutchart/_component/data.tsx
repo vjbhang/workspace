c3f4e6633ce87978344c3ef0c093d69a80136ea9
vjbhang <bhangjiwon@pm.me>
2023-09-06 19:18:50 +0900

    init donut chart generator

---
+++ packages/d3-react-next/src/app/custom-d3/donutchart/_component/data.tsx
@@ -0,0 +1,34 @@
+import { DonutChartData } from "./donutchartGenerator";
+
+export const turtleNeckPostureMockData: DonutChartData[] = [
+  {
+    name: "10대",
+    value: 7900,
+    percentage: 79,
+  },
+  {
+    name: "20대",
+    value: 2100,
+    percentage: 21,
+  },
+  {
+    name: "30대",
+    value: 300,
+    percentage: 3,
+  },
+  {
+    name: "40대",
+    value: 200,
+    percentage: 2,
+  },
+  {
+    name: "50대",
+    value: 300,
+    percentage: 3,
+  },
+  {
+    name: "60대",
+    value: 200,
+    percentage: 2,
+  },
+];
